6cc9a5c25f45cb9ec764ac0b3c941e71c2fea958
VergaraC <victorvaac@al.insper.edu.br>
2021-03-16 18:56:59 -0300

    Core Dumped ainda

--- geometry.c
+++ geometry.c
@@ -6,7 +6,10 @@ int verify(point p, point a, point b) {
     
     //point p2;
     //eq de reta: mx + n
-    int m = (a.y-b.y)/(a.x-b.x); //tg da linha
+    int m = 0;
+    if (a.x-b.x==0){
+        m = (a.y-b.y)/(a.x-b.x); //tg da linha
+    }
     int n = a.y-m*a.x; //constante da eq
     point p2;
     p2.y = p.y;
@@ -17,8 +20,8 @@ int verify(point p, point a, point b) {
     }
     
     
-    //if(((p2.y < a.y && p2.y > b.y) || (p2.y < b.y && p2.y > a.y)) && ((p2.x < a.x && p2.x > b.x) || (p2.x < b.x && p2.x > a.x))){
-    if((p2.y < a.y && p2.y > b.y) || (p2.y < b.y && p2.y > a.y)){
+    if(((p2.y < a.y && p2.y > b.y) || (p2.y < b.y && p2.y > a.y)) && ((p2.x < a.x && p2.x > b.x) || (p2.x < b.x && p2.x > a.x))){
+    //if((p2.y < a.y && p2.y > b.y) || (p2.y < b.y && p2.y > a.y)){
         return 1;
     }
     return 0;
