b98bd086736a009c540f9604bda145fce172a93b
VergaraC <victorvaac@al.insper.edu.br>
2021-03-16 22:00:53 -0300

    d

--- geometry.c
+++ geometry.c
@@ -7,19 +7,19 @@ int verify(point p, point a, point b) {
     //point p2;
     //eq de reta: mx + n
     int m = 0;
-    if (a.x-b.x==0){
-        m = (a.y-b.y)/(a.x-b.x); //tg da linha
+    int c = a.x-b.x;
+    if (c > 0.000001){
+        m = (a.y-b.y)/(c); //tg da linha
     }
     int n = a.y-m*a.x; //constante da eq
     point p2;
     p2.y = p.y;
-    if (m == 0 ){
+    if (m != 0 ){
         p2.x = (p2.y - n)/m;
     }else{
         p2.x = a.x;
     }
-    
-    
+   
     if(((p2.y < a.y && p2.y > b.y) || (p2.y < b.y && p2.y > a.y)) && ((p2.x < a.x && p2.x > b.x) || (p2.x < b.x && p2.x > a.x))){
     //if((p2.y < a.y && p2.y > b.y) || (p2.y < b.y && p2.y > a.y)){
         return 1;
