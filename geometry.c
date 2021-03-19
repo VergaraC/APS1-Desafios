d2a4919e455bc9333e481f6b725f88ca67f98e24
VergaraC <victorvaac@al.insper.edu.br>
2021-03-19 10:23:38 -0300

    Toca de nome de variaveis para boas praticas

--- geometry.c
+++ geometry.c
@@ -6,11 +6,11 @@ double condicao_especial = 0.000001;
 
 int verify(point p, point a, point b) {
     
-    point p2, projecao, pXmin, pXmin2;
+    point p2, projecao, p_x_min, p_x_min2;
 
     //eq de reta: mx + n
     int m;
-    int bigY,smallY, xSmallY,xBigY, bigX, yBigX, smallX, ySmallx;
+    int big_y,small_y, x_small_y,x_big_y, big_x, y_big_x, small_x, y_small_x;
     int c = a.x-b.x;
     
     if ((c > condicao_especial) || (c< -condicao_especial)){
@@ -19,29 +19,29 @@ int verify(point p, point a, point b) {
         m = 0;
     }
     if (a.y >= b.y){
-        bigY = a.y;
-        xBigY = a.x;
-        smallY = b.y;
-        xSmallY = b.x;
+        big_y = a.y;
+        x_big_y = a.x;
+        small_y = b.y;
+        x_small_y = b.x;
     }else{
-        bigY = b.y;
-        xBigY =b.x;
-        smallY = a.y;
-        xSmallY = a.x;
+        big_y = b.y;
+        x_big_y =b.x;
+        small_y = a.y;
+        x_small_y = a.x;
     }
     if (a.x >= b.x){
-        bigX = a.x;
-        yBigX = a.y;
-        smallX = b.x;
-        ySmallx = b.y;
+        big_x = a.x;
+        y_big_x = a.y;
+        small_x = b.x;
+        y_small_x = b.y;
     }else{
-        bigX = b.x;
-        yBigX = b.y;
-        smallX = a.x;
-        ySmallx = a.y;
+        big_x = b.x;
+        y_big_x = b.y;
+        small_x = a.x;
+        y_small_x = a.y;
 
     }
-    printf("BigX : %d, xBigY: %d, xSmallY: %d,   \n", bigX, xBigY,xSmallY);
+    printf("Big_x : %d, x_big_y: %d, x_small_y: %d,   \n", big_x, x_big_y,x_small_y);
     int n = a.y-m*a.x; //constante da eq
     p2.y = p.y;
 
@@ -53,24 +53,24 @@ int verify(point p, point a, point b) {
     projecao.x = p.x;
     projecao.y = m*projecao.x + n;
 
-    pXmin2.x = smallX;
-    pXmin2.y = yBigX;
-    pXmin.x = smallX;
-    pXmin.x = ySmallx; 
+    p_x_min2.x = small_x;
+    p_x_min2.y = y_big_x;
+    p_x_min.x = small_x;
+    p_x_min.x = y_small_x; 
 
 
     printf(" Projecao: %d, %d \n", projecao.x, projecao.y);
     printf("p2: %d, %d   P:  %d, %d   A: %d, %d  B: %d, %d \n" , p2.x,p2.y,p.x,p.y, a.x, a.y, b.x, b.y);
     
-    if ( (p2.x == p.x) && (p.y <= bigY && p.y >= smallY) && (!(p.y > smallY && p.y < bigY && p.x == smallX))|| (p.x == smallX && p.x == ySmallx)){
-        return 2; //(p2.x == xBigY && p2.y == bigY )
+    if ( (p2.x == p.x) && (p.y <= big_y && p.y >= small_y) && (!(p.y > small_y && p.y < big_y && p.x == small_x))|| (p.x == small_x && p.x == y_small_x)){
+        return 2; //(p2.x == x_big_y && p2.y == big_y )
     }else if((p.y == a.y && p.y == b.y)){
         return 2;
-    }else if ( (p2.y == a.y && p2.x == a.x) || ((p2.y == b.y) && (p2.x == b.x) && (p2.y < bigY)) && ((p.y <= smallY))){
+    }else if ( (p2.y == a.y && p2.x == a.x) || ((p2.y == b.y) && (p2.x == b.x) && (p2.y < big_y)) && ((p.y <= small_y))){
         return 0;
     
-    }else if( (p2.y <= bigY && p2.y >= smallY) && ( (p2.x <= bigX && p2.x >= smallX) && (p.x < bigX) ) && !(p.y == smallY && p.y < bigY)){
-        //if (m==0 ||verify(p, pXmin, pXmin2)==1){
+    }else if( (p2.y <= big_y && p2.y >= small_y) && ( (p2.x <= big_x && p2.x >= small_x) && (p.x < big_x) ) && !(p.y == small_y && p.y < big_y)){
+        //if (m==0 ||verify(p, p_x_min, p_x_min2)==1){
             return 1;
        // }
         
@@ -84,28 +84,28 @@ int inside(point p, point poly[], int n) {
         point ponto2 = poly[1];
         point ponto3 = poly[2];
         point ponto4 = poly[3];
-        int xMin = 100000000;
-        int xMax = 0;
-        int yMin = 100000000;
-        int yMax = 0;
+        int x_min = 100000000;
+        int x_max = 0;
+        int y_min = 100000000;
+        int y_max = 0;
         for (int j=0; j<n; j++){
-            if(poly[j].x > xMax){
-                xMax = poly[j].x;
+            if(poly[j].x > x_max){
+                x_max = poly[j].x;
             } else{
-                yMax = poly[j].y;
+                y_max = poly[j].y;
             }
-            if(poly[j].x < xMin){
-                xMin = poly[j].x;
+            if(poly[j].x < x_min){
+                x_min = poly[j].x;
             } else{
-                yMin = poly[j].y;
+                y_min = poly[j].y;
             }
         }
-        printf("xMin: %d, yMin: %d, xMax: %d, yMax: %d \n", xMin, yMin, xMax, yMax);
-        if((ponto1.x == xMin && ponto1.y == yMin) && (ponto2.x == xMin && ponto2.y == yMax) && (ponto3.x == xMax && ponto3.y == yMax) && (ponto4.x == xMax && ponto4.y == yMin)){
+        printf("x_min: %d, y_min: %d, x_max: %d, y_max: %d \n", x_min, y_min, x_max, y_max);
+        if((ponto1.x == x_min && ponto1.y == y_min) && (ponto2.x == x_min && ponto2.y == y_max) && (ponto3.x == x_max && ponto3.y == y_max) && (ponto4.x == x_max && ponto4.y == y_min)){
             printf("quadrado \n");
         }
 
-        if((p.x < xMax && p.x >= xMin) && (p.y >= yMin && p.y <= yMax)){
+        if((p.x < x_max && p.x >= x_min) && (p.y >= y_min && p.y <= y_max)){
             return 1;
         }
         
